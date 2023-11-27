faa8c01ed87bf98d75c6c86a1c340073bc1e18b7
Seba <sebastiantorresola3@gmail.com>
2023-11-27 12:02:43 -0800

    Added home page button to chat page and removed unused styles

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,37 +1,41 @@
 import React from 'react';
 import g10 from "../assets/g10.svg";
 import profile from"../assets/profile-logo-image.jpeg";
-import '../Chat.css';
+import './Chat.css';
+import { Link } from 'react-router-dom';
 export default function Chat(){
     return (
-        
-    <div className="side-bar">
+    <> 
+        <div className='page-container'>
+            <div className="side-bar">
 
-    <div className="header-title"> 
-        <div className="intro-text">BeavsAI</div>
-        <div className="intro-image"><img src={g10}/></div>
-    </div>
-       
-    <div className="chat-box-container">
-       <div className="side-box-1">Recent Request</div>
-       <div className="side-box-2">Recent Request</div>
-       <div className="side-box-3">Recent Request</div>
-       <div className="side-box-4">Recent Request</div>
-       <div className="side-box-5">Recent Request</div>
-       <div className="side-box-6">Recent Request</div>
-    </div>
-    
+                <div className="header-title"> 
+                    <h1 className="intro-text">BeavsAI</h1>
+                    <div className="intro-image">
+                        <img src={g10}/>
+                    </div>
+                </div>
+                
+                <div className="chat-box-container">
+                    <h2 className="side-box-1">Recent Request</h2>
+                    <h2 className="side-box-2">Recent Request</h2>
+                    <h2 className="side-box-3">Recent Request</h2>
+                    <h2 className="side-box-4">Recent Request</h2>
+                    <h2 className="side-box-5">Recent Request</h2>
+                    <h2 className="side-box-6">Recent Request</h2>
+                </div>
+                
 
-    <div className="profile-container">
-        <div className="profile-icon"><img src={profile}/></div>
-        <h1 className="username">User</h1>
-    </div>
+                <div className="profile-container">
+                    <div className="profile-icon"><img src={profile}/></div>
+                    <h1 className="username">User</h1>
+                </div>
 
+            </div>
 
-
-
-
-    </div>
+            <Link to='/' className='btn'>Back</Link>
+        </div>
+    </>   
         
     )
 }
