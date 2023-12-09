87bfe9dc7c6769368884d084a67f9923caa5e778
Thomas <nyumat18@gmail.com>
2023-12-09 00:41:20 -0800

    🌋 refactor: -- tailwind config comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,13 @@ export default {
     },
     plugins: [daisyui],
     daisyui: {
-        themes: ["dark"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-        darkTheme: "dark", // name of one of the included themes for dark mode
-        base: false, // applies background color and foreground color for root element by default
-        styled: true, // include daisyUI colors and design decisions for all components
-        utils: true, // adds responsive and modifier utility classes
-        prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-        logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-        themeRoot: ":root", // The element that receives theme color CSS variables
+        themes: ["dark"], 
+        darkTheme: "dark",
+        base: false, 
+        styled: true,
+        utils: true,
+        prefix: "",
+        logs: true,
+        themeRoot: ":root",
     },
 }
